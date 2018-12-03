d39ea1d5cdf792cbf70a8b254da11bcc8f793610
Mustafa Celalettin KARAGÜLLE <mustafacelalettinkaragulle@gmail.com>
2018-12-03 12:19:26 +0000

    config eklendi.

---
+++ src/ImageResizerFacade.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ImageResizer;
+
+/**
+ * This file is part of Laratrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Laratrust
+ */
+
+use Illuminate\Support\Facades\Facade;
+
+class ImageResizerFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'imageresizer';
+    }
+}
---
+++ src/config/ImageResizerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace ImageResizer;
+use Illuminate\Support\ServiceProvider;
+class LaratrustServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/imageresizer.php' => config_path('imageresizer.php')
+        ], 'config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
